1d342aeca631e415b10c3c07b666bba34a281455
Ezequiel Martin <ezequielmartin92@gmail.com>
2025-04-08 20:32:07 -0300

    Implemento las funciones para crear notas y cambiar su importancia, las muevo a noteReducer.js y las importo en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import noteReducer from "./reducers/noteReducer"
+import { createNote } from './reducers/noteReducer'
+import { toggleImportanceOf } from './reducers/noteReducer'
 
 import { createStore } from 'redux'
 
@@ -26,11 +28,27 @@ store.dispatch({
 })
 
 const App = () => {
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch(createNote(content))
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch(toggleImportanceOf(id))
+  }
+
   return(
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
       <ul>
         {store.getState().map(note=>
-          <li key={note.id}>
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         )}
